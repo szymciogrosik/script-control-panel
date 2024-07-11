d6ac220f3a8414e7613947bd243fea773350a125
Szymon Gross <szymon.jaroslaw.gross@gmail.com>
2024-07-11 20:11:00 +0200

    Updated conformation popup

--- src/main/java/org/codefromheaven/controller/ConfirmationPopupController.java
+++ src/main/java/org/codefromheaven/controller/ConfirmationPopupController.java
@@ -4,10 +4,13 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
 import org.codefromheaven.service.animal.AnimalService;
 
 public class ConfirmationPopupController {
@@ -19,31 +22,37 @@ public class ConfirmationPopupController {
     }
 
     public void setupPage() {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Confirmation");
         alert.setHeaderText(null);
         alert.setWidth(300);
 
-        // Add an image to the window bar
-        ImageView imageView = new ImageView(AnimalService.getInstance().getRandomAnimalImage());
-        imageView.setFitWidth(50);
-        imageView.setFitHeight(50);
-        alert.setGraphic(imageView);
-
-        // Create a VBox to hold the text vertically centered
-        VBox vbox = new VBox();
-        vbox.setAlignment(Pos.CENTER);
-        vbox.setSpacing(10);
+        // Create an HBox to hold the image and text side by side
+        HBox hbox = new HBox();
+        hbox.setAlignment(Pos.CENTER_LEFT);
+        hbox.setSpacing(10);
 
         // Add the confirmation message text
         Text text = new Text(confirmationMessage);
-        vbox.getChildren().add(text);
+
+        // Add text and image to the HBox
+        hbox.getChildren().addAll(text);
+
+        // Create a VBox to center the HBox vertically
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(10);
+        vbox.getChildren().add(hbox);
 
         // Set the VBox as the content of the dialog pane
         alert.getDialogPane().setContent(vbox);
 
         ButtonType buttonTypeYes = new ButtonType("OK", ButtonBar.ButtonData.YES);
         alert.getButtonTypes().setAll(buttonTypeYes);
+
+        // Set the window icon
+        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(AnimalService.getInstance().getRandomAnimalImage());
 
         alert.showAndWait();
     }
