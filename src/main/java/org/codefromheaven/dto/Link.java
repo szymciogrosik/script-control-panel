5efa7546342af9c1d10d57c11fd18d7fd831fedd
Szymon Gross <szyg@netcompany.com>
2024-07-22 17:30:34 +0200

    Added configuration files

--- src/main/java/org/codefromheaven/dto/Link.java
+++ src/main/java/org/codefromheaven/dto/Link.java
@@ -6,7 +6,7 @@ public enum Link {
     GH_PROJECT( ABOUT_AUTHOR.getUrl() + "/script-control-panel"),
     GH_RELEASES(GH_PROJECT.getUrl() + "/releases"),
     WIKI(GH_PROJECT.getUrl() + "/wiki"),
-    WIKI_CONFIGURATION(WIKI.getUrl() + "/"),
+    WIKI_CONFIGURATION(WIKI.getUrl() + "/Configuration"),
     PIN_JAR_TO_TASKBAR(WIKI.getUrl() + "/Guide-how-to-pin-executable-JAR-to-the-taskbar-or-Windows-start-for-Windows-11"),
     ISSUES(GH_PROJECT.getUrl() + "/issues"),
     API_ALL_RELEASES("https://api.github.com/repos/szymciogrosik/script-control-panel/releases");
