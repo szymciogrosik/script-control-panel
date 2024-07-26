e889aaf28219c9707c5bb78390b2caa1a7e94fd2
Szymon Gross <szyg@netcompany.com>
2024-07-26 08:44:16 +0200

    Added new animals

--- src/main/java/org/codefromheaven/resources/AnimalNamesProvider.java
+++ src/main/java/org/codefromheaven/resources/AnimalNamesProvider.java
@@ -19,30 +19,39 @@ public class AnimalNamesProvider {
     * */
     public static final List<String> ALL = Arrays.asList(
             "armadillo.png",
+            "axolotl.png",
             "bat.png",
+            "bear.png",
             "bee.png",
+            "boar.png",
             "cat.png",
             "chameleon.png",
             "chick.png",
             "crab.png",
+            "crocodile.png",
             "dinosaur.png",
+            "diplodocus.png",
             "dolphin.png",
             "elephant.png",
             "fox.png",
             "frog.png",
             "giraffe.png",
             "koala.png",
+            "octopus.png",
             "owl.png",
             "panda.png",
             "penguin.png",
             "raccoon.png",
             "rat.png",
             "sea-turtle.png",
+            "sheep.png",
+            "shrimp.png",
             "snake.png",
             "squirrel.png",
             "starfish.png",
             "turtle.png",
-            "whale.png"
+            "whale.png",
+            "wolf.png"
     );
 
 }
