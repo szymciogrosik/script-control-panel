3f749b6d75be4c797e6c415f65d641840dcc2fb3
Szymon Gross <szyg@netcompany.com>
2024-07-26 13:57:11 +0200

    Refactored code to use also local tokens.

--- src/main/java/org/codefromheaven/service/gh/GithubService.java
+++ src/main/java/org/codefromheaven/service/gh/GithubService.java
@@ -9,37 +9,46 @@ import java.util.Properties;
 
 public class GithubService {
 
+    private static final String PROPERTIES_LOCAL_FILE_NAME_PREFIX = "local.";
+    private static final String PROPERTIES_FILE_NAME = "gh.token.properties";
+    private static final String GH_RELEASE_FETCH_RELEASES_TOKEN_NAME = "ghReleaseFetchReleasesToken";
+    private static final String GH_RELEASE_DOWNLOAD_TOKEN_NAME = "ghReleaseDownloadToken";
+
     private GithubService() {
     }
 
-    public static Optional<String> getGhDownloadToken() {
-        Properties properties = new Properties();
-        try (InputStream input = FileNamesLoader.getResourceAsStream("gh.token.properties")) {
-            if (input == null) {
-                throw new RuntimeException("Sorry, unable to find version.properties");
-            }
-            properties.load(input);
-            String token = properties.getProperty("ghReleaseDownloadToken");
+    public static Optional<String> getGhGetReleases() {
+        Optional<String> localToken = getLocalToken(PROPERTIES_FILE_NAME, GH_RELEASE_FETCH_RELEASES_TOKEN_NAME);
+        if (localToken.isPresent()) {
+            return localToken;
+        }
+        return getToken(PROPERTIES_FILE_NAME, GH_RELEASE_FETCH_RELEASES_TOKEN_NAME);
+    }
 
-            if (token == null || token.isEmpty()) {
-                return Optional.empty();
-            } else {
-                return Optional.of(token);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            throw new RuntimeException(ex);
+    public static Optional<String> getGhDownloadToken() {
+        Optional<String> localToken = getLocalToken(PROPERTIES_FILE_NAME, GH_RELEASE_DOWNLOAD_TOKEN_NAME);
+        if (localToken.isPresent()) {
+            return localToken;
+        }
+        return getToken(PROPERTIES_FILE_NAME, GH_RELEASE_DOWNLOAD_TOKEN_NAME);
+    }
+
+    private static Optional<String> getLocalToken(String fileName, String tokenName) {
+        try {
+            return getToken(PROPERTIES_LOCAL_FILE_NAME_PREFIX + fileName, tokenName);
+        } catch (Exception ex) {
+            return Optional.empty();
         }
     }
 
-    public static Optional<String> getGhGetReleases() {
+    private static Optional<String> getToken(String fileName, String tokenName) {
         Properties properties = new Properties();
-        try (InputStream input = FileNamesLoader.getResourceAsStream("gh.token.properties")) {
+        try (InputStream input = FileNamesLoader.getResourceAsStream(fileName)) {
             if (input == null) {
                 throw new RuntimeException("Sorry, unable to find version.properties");
             }
             properties.load(input);
-            String token = properties.getProperty("ghReleaseFetchReleasesToken");
+            String token = properties.getProperty(tokenName);
 
             if (token == null || token.isEmpty()) {
                 return Optional.empty();
