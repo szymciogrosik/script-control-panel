115436d7b3d5da613d45a5618a82e7cd30cf4b59
szymciogrosik <szymon.jaroslaw.gross@gmail.com>
2025-06-19 01:16:19 +0200

    Added cyber-punk-style.css.

--- src/main/java/org/codefromheaven/dto/Style.java
+++ src/main/java/org/codefromheaven/dto/Style.java
@@ -2,7 +2,8 @@ package org.codefromheaven.dto;
 
 public enum Style {
 
-    CLASSIC("classic-style.css");
+    CLASSIC("classic-style.css"),
+    CYBER_PUNK("cyber-punk-style.css");
 
     private final String fileName;
 
